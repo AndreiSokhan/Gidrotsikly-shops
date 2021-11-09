9fe6652070dd06d631fe39d9df906910f0757881
andreisokhan <sokhanpageproofs@gmail.com>
2021-11-09 19:15:31 +0300

    refactoring header & on fonts in cards catalog

--- src/components/header/headerBottom/HeaderBottom.jsx
+++ src/components/header/headerBottom/HeaderBottom.jsx
@@ -8,7 +8,7 @@ function HeaderBottom(props) {
             <ul className={s.list}>
                <li className={s.item}><NavLink to={props.data1.link}>Квадроциклы</NavLink></li>
                <li className={s.item}><NavLink to={props.data2.link}>Катера</NavLink></li>
-               <li className={s.item}><NavLink to={props.data3.link}>Гидроциклы</NavLink></li>
+               <li className={`${s.item} ${s.active}`}><NavLink to={props.data3.link}>Гидроциклы</NavLink></li>
                <li className={s.item}><NavLink to={props.data4.link}>Лодки</NavLink></li>
                <li className={s.item}><NavLink to={props.data5.link}>Вездеходы</NavLink></li>
                <li className={s.item}><NavLink to={props.data6.link}>Снегоходы</NavLink></li>
