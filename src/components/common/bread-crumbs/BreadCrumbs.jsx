bb8d7e1263078cc3b04cdbe229df5c68e6661cfa
andreisokhan <sokhanpageproofs@gmail.com>
2021-11-16 23:16:29 +0300

    breadcrumbs add

--- src/components/common/bread-crumbs/BreadCrumbs.jsx
+++ src/components/common/bread-crumbs/BreadCrumbs.jsx
@@ -1,16 +1,28 @@
-import s from './BreadCrumbs.module.scss';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 // хлебные крошки (показывают где находишься на сайте)
 
-function BreadCrumbs() {
-
-    return (
-
-        <div className={s.breadCrumbs}>
-        </div>
-
-    )
-
+function handleClick(event) {
+    event.preventDefault();
+    console.info('You clicked a breadcrumb.');
 }
 
-export default BreadCrumbs;
+export default function CustomSeparator() {
+    const breadcrumbs = [
+    <Link underline="hover" key="1" color="#C4C4C4" href="#" onClick={handleClick}>Главная</Link>,
+    <Typography key="2" color="#C4C4C4">Гидроциклы</Typography>,
+];
+
+return (
+    <Stack spacing={2}>
+    <Breadcrumbs
+        separator={<NavigateNextIcon fontSize="small" />}
+        aria-label="breadcrumb" color="#C4C4C4">{breadcrumbs}
+    </Breadcrumbs>
+    </Stack>
+);
+}
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -29,19 +29,17 @@ function Footer() {
    return (
       <div className={s.container}>
          <div className={s.footer}>
-
             <div className={s.wrapper}>
                <p className={s.text}>Подпишитесь на нашу рассылку и узнавайте о акциях быстрее</p>
                <div className={s.inner}>
                <input className={s.input} type="email" placeholder="Введите ваш e-mail:" ></input>
                <button className={s.btn}>отправить</button>
-               </div>
-               
+               </div> 
             </div>
 
    
-      <FooterList title={dataList1.title} item1={dataList1.item1} item2={dataList1.item2} item3={dataList1.item3} item4={dataList1.item4} bottom={dataList1.bottom}/>
-         <FooterList title={dataList2.title} item1={dataList2.item1} item2={dataList2.item2} item3={dataList2.item3} item4={dataList2.item4} bottom={dataList2.bottom}/>
+            <FooterList title={dataList1.title} item1={dataList1.item1} item2={dataList1.item2} item3={dataList1.item3} item4={dataList1.item4} bottom={dataList1.bottom}/>
+            <FooterList title={dataList2.title} item1={dataList2.item1} item2={dataList2.item2} item3={dataList2.item3} item4={dataList2.item4} bottom={dataList2.bottom}/>
       
             <div className={s.social}>
                <a className={s.socialLink} href="/">
@@ -57,8 +55,6 @@ function Footer() {
                   <img className={s.socialImg} src={youtube} alt="Youtube" />
                </a>
             </div>
-      
-
    
          </div>
       </div>
