91451476a0bd2c64772702031f6b0ba6f786599a
AlesyaOsipenko <alesya-osipenko@mail.ru>
2021-11-17 22:01:09 +0300

    refactoring JetSkisCatalog

--- src/components/common/pagination/Pagination.jsx
+++ src/components/common/pagination/Pagination.jsx
@@ -6,7 +6,7 @@ import Stack from '@mui/material/Stack';
 export default function BasicPagination() {
    return (
       <Stack spacing={2}>
-      <Pagination className={s.pagination} count={11}  shape="rounded" hidePrevButton hideNextButton variant="outlined" color="primary"/>
+      <Pagination className={s.pagination} count={11}  shape="rounded" hidePrevButton hideNextButton variant="outlined"/>
       </Stack>
    );
 }
--- src/content2/main/jetSkisCatalog/JetSkisCatalog.jsx
+++ src/content2/main/jetSkisCatalog/JetSkisCatalog.jsx
@@ -13,6 +13,8 @@ import img10 from '../../images/ten.jpg';
 import img11 from '../../images/eleven.jpg';
 import img12 from '../../images/twelve.jpg';
 
+
+
 // главный блок с контентом на второй странице нашего проекта
 
 function JetSkisCatalog() {
@@ -21,104 +23,90 @@ function JetSkisCatalog() {
       {
          img: img1,
          title: "Гидроцикл BRP SeaDoo GTI 130hp SE BlackMango",
-         span: "9 800 ₽",
+         span: "1 049 500 ₽",
          buyButton: true
       
       },
       {
          img: img2,
-         title: "Гидроцикл BRP SeaDoo GTI 130hp SE BlackMango",
-         span: "6 900 ₽",
-         buyButton: true
+         title: "Гидроцикл BRP SeaDoo GTI 155hp SE Long Blue Metallic",
+         span: "1 100 475 ₽",
+         buyButton: true, 
+         label: "SALE"
          
       },
       {
          img: img3,
-         title: "Гидроцикл BRP SeaDoo GTI 130hp SE BlackMango",
-         span: "68 000 ₽",
-         buyButton: false
+         title: "Гидроцикл BRP SeaDoo GTR 230hp X California green",
+         span: "1 323 700 ₽",
+         buyButton: true
       },
       {
          img: img4,
-         title: "Гидроцикл BRP SeaDoo GTI 130hp SE BlackMango",
+         title: "Гидроцикл BRP SeaDoo GTR 230hp STD Black / Gulfstream",
          span: "9 800 ₽",
-         buyButton: true
+         buyButton: false,
+         label: "SALE"
       
       },
       {
          img: img5,
-         title: "Гидроцикл BRP SeaDoo GTI 130hp SE BlackMango",
-         span: "9 800 ₽",
+         title: "Гидроцикл BRP SeaDoo GTX 300hp LTD Liquid Metal",
+         span: "1 543 000 ₽",
          buyButton: true
       
       },
       {
          img: img6,
-         title: "Гидроцикл BRP SeaDoo GTI 130hp SE BlackMango",
-         span: "9 800 ₽",
+         title: "Гидроцикл BRP SeaDoo Spark 60hp 2 up",
+         span: "732 345 ₽",
          buyButton: true
       
       },
       {
          img: img7,
-         title: "Гидроцикл BRP SeaDoo GTI 130hp SE BlackMango",
-         span: "9 800 ₽",
+         title: "Гидроцикл BRP SeaDoo Spark GTS 90hp Rental",
+         span: "857 666 ₽",
          buyButton: true
       
       },
       {
          img: img8,
-         title: "Гидроцикл BRP SeaDoo GTI 130hp SE BlackMango",
-         span: "9 800 ₽",
-         buyButton: true
+         title: "Гидроцикл BRP SeaDoo WAKE 230hp PRO Teal blue",
+         span: "1 229 711 ₽",
+         buyButton: true,
+         label: "SALE"
       
       },
       {
          img: img9,
-         title: "Гидроцикл BRP SeaDoo GTI 130hp SE BlackMango",
-         span: "9 800 ₽",
+         title: "Гидроцикл Spark 2-UP 900 Ho Ace Chili Pepper",
+         span: "587 440 ₽",
          buyButton: true
       
       },
       {
          img: img10,
-         title: "Гидроцикл BRP SeaDoo GTI 130hp SE BlackMango",
-         span: "9 800 ₽",
+         title: "Гидроцикл Spark 2-UP 900 Ho Ace Pineapple",
+         span: "587 440 ₽",
          buyButton: true
       
       },
       {
          img: img11,
-         title: "Гидроцикл BRP SeaDoo GTI 130hp SE BlackMango",
+         title: "Гидроцикл BRP Sea-doo Spark 2-UP 900 Ace Vanilla",
          span: "9 800 ₽",
-         buyButton: true
+         buyButton: false,
+         label: "SALE"
       
       },
       {
          img: img12,
-         title: "Гидроцикл BRP SeaDoo GTI 130hp SE BlackMango",
+         title: "Гидроцикл Spark 3-UP 900 HO Ace IBR Blueberry",
          span: "9 800 ₽",
-         buyButton: true
+         buyButton: false
       
-      },
-      {
-         img: img1,
-         title: "Гидроцикл BRP SeaDoo GTI 130hp SE BlackMango",
-         span: "9 800 ₽",
-         buyButton: true
-      },
-      {
-         img: img1,
-         title: "Гидроцикл BRP SeaDoo GTI 130hp SE BlackMango",
-         span: "9 800 ₽",
-         buyButton: true
-      },
-      {
-         img: img1,
-         title: "Гидроцикл BRP SeaDoo GTI 130hp SE BlackMango",
-         span: "9 800 ₽",
-         buyButton: true
-      },
+      }
       
    ]
 
@@ -126,7 +114,7 @@ function JetSkisCatalog() {
 
       <div className={s.jetSkisCatalog}>
          {data.map(item => {
-            return <ProductCard img={item.img} title={item.title} span={item.span} buyButton={item.buyButton}/>
+            return <ProductCard img={item.img} title={item.title} span={item.span} buyButton={item.buyButton} label={item.label}/>
          })}
       </div>
 
