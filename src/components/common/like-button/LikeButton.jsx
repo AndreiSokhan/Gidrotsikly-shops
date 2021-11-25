ceb6abf2ab61df8c3b0158f719a75a697c954cb9
AlesyaOsipenko <alesya-osipenko@mail.ru>
2021-11-26 02:11:03 +0300

    refactoring

--- src/components/common/like-button/LikeButton.jsx
+++ src/components/common/like-button/LikeButton.jsx
@@ -6,11 +6,29 @@ import Favorite from '@mui/icons-material/Favorite';
 
 //кнопка в форме сердца для выбора товара
 
-function LikeButton (props) {
+// const BpIcon = styled('span')(({ theme }) => ({
+//     borderRadius: 3,
+//     width: 16,
+//     height: 16,
+//     '.Mui-focusVisible &': {
+//         outline: '2px auto rgba(19,124,189,.6)',
+//         outlineOffset: 2,
+//     },
+//     'input:hover ~ &': {
+//         backgroundColor: theme.palette.mode === 'dark' ? '#30404d' : '#ebf1f5',
+//     },
+//     'input:disabled ~ &': {
+//         boxShadow: 'none',
+//         background:
+//             theme.palette.mode === 'dark' ? 'rgba(57,75,89,.5)' : 'rgba(206,217,224,.5)',
+//     },
+// }));
+
+function LikeButton(props) {
 
     return (
-        <Checkbox style={props.style} icon={<FavoriteBorder />} checkedIcon={<Favorite/>}
+        <Checkbox style={props.style} icon={<FavoriteBorder />} checkedIcon={<Favorite />}
         />
     )
 }
-    export default LikeButton;
+export default LikeButton;
