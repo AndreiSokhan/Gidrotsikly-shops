7eca8e07c76d9297967a956f6dccb3817fc427f0
andreisokhan <sokhanpageproofs@gmail.com>
2021-11-26 22:42:28 +0300

    table refactoring

--- src/content3/about/aboutTop/AboutTop.jsx
+++ src/content3/about/aboutTop/AboutTop.jsx
@@ -10,7 +10,7 @@ function AboutTop(props) {
          <li className={s.item}><NavLink className={s.link} to={props.data1.link}>О товаре</NavLink></li>
          <li className={s.item}><NavLink className={s.link} to={props.data2.link}>Характеристики</NavLink></li>
          <li className={s.item}><NavLink className={s.link} to={props.data3.link}>Отзывы</NavLink></li>
-         <li className={`${s.item} ${s.active}`}><NavLink className={s.link} to={props.data4.link}>Самовывоз</NavLink></li>
+         <li className={s.item}><NavLink className={`${s.link} ${s.active}`} to={props.data4.link}>Самовывоз</NavLink></li>
          <li className={s.item}><NavLink className={s.link} to={props.data5.link}>Доставка</NavLink></li>
          <li className={s.item}><NavLink className={s.link} to={props.data6.link}>Cервис</NavLink></li>
          <li className={s.item}><NavLink className={s.link} to={props.data7.link}>Гарантия</NavLink></li>
